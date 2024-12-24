b50222c498223d1ca8e78ab46f872db2ca44b3be
Oleh <zaaqq135@gmail.com>
2024-12-24 23:56:54 +0200

    init project

---
+++ src/main/java/app/snake/App.java
@@ -0,0 +1,13 @@
+package app.snake;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        new GameFrame();
+    }
+}
---
+++ src/main/java/app/snake/GamePanel.java
@@ -0,0 +1,25 @@
+package app.snake;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements ActionListener {
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO
+    }
+
+    public class MyKeyAdapter extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+            
+        }
+    }
+
+}
